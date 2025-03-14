c7d1ec7a055042cb3c75f6f8a5395a5d11018307
zhenglin huang <147322689+hzlsaber@users.noreply.github.com>
2025-03-14 11:14:49 +0000

    Update run_SIDA_training.sh

--- run_SIDA_training.sh
+++ run_SIDA_training.sh
@@ -1,13 +1,10 @@
-
-#!/bin/bash
-# Run the training script with DeepSpeed directly specifying GPU 1
 deepspeed --include localhost:1 --master_port=24999 train_SIDA.py \
-  --version="./ck/LISA-7B-v1" \
-  --dataset_dir='/LOCAL2/zhenglin/benchmark' \
-  --vision_pretrained="/LOCAL2/zhenglin/LISA/ck/sam_vit_h_4b8939.pth" \
-  --val_dataset="/LOCAL2/zhenglin/benchmark/"\
+  --version="/path_to/LISA-7B-v1" \
+  --dataset_dir='/path_to/benchmark' \
+  --vision_pretrained="/path_to/sam_vit_h_4b8939.pth" \
+  --val_dataset="/path_to/benchmark/"\
   --batch_size=2 \
-  --exp_name="SIDA-7B-test" \
+  --exp_name="SIDA-7B" \
   --epochs=10 \
   --steps_per_epoch=1000 \
   --lr=0.0001 \
